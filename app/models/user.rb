fa352e3321adb4970f8265412a218ec274091947
EC2 Default User <ec2-user@ip-172-31-36-118.ec2.internal>
2020-08-14 12:16:08 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts,dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -9,7 +10,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence:true, length: { minimum: 6 }, allow_nil: true
-  
+
   #渡されたハッシュ文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -63,6 +64,12 @@ class User < ApplicationRecord
   
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  #試作feedの定義
+  #完全な実証は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?",id)
   end
   
   private
